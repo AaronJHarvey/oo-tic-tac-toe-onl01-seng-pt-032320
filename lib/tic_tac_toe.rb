5ead69b43c3bcb485b64f1809d667ee91e2182f9
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-20 23:19:25 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,33 @@
+class TicTacToe
+def initialize(board = nil)
+  @board = board || Array.new(9," ")
+end
+
+WIN_COMBINATIONS = [
+[0,1,2],
+[0,3,6],
+[0,4,8],
+[1,4,7],
+[2,5,8],
+[2,4,6],
+[3,4,5],
+[6,7,8]
+]
+  
+def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(num)
+   num.to_i-1
+  end
+  
+  def move (index,token = "X")
+@board[index] = token
+  end
+  
+end
