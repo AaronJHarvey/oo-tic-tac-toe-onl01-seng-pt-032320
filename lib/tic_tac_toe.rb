d137033aa489521a273009a722706c42248eb9d6
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-21 00:01:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,12 @@ def turn
 end
 
 def won?
-  if 
+WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
 
 
 end
